75854abcc6ef6ca188136f1085663b1b6e397796
VictorHFMartins <navictor70@gmail.com>
2025-03-21 16:23:08 -0300

    Resolução do terceiro exercício da lista de while.

--- PSC/lista_de_exercicios/lista_while/ex_3.java
+++ PSC/lista_de_exercicios/lista_while/ex_3.java
@@ -1,7 +1,24 @@
+// Crie um programa para ler 3 notas e mostrar a média delas.
+
 package PSC.lista_de_exercicios.lista_while;
+
+import java.util.Scanner;
 
 public class ex_3 {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        Scanner sc = new Scanner(System.in);
+        int cont = 0;
+        int notas = 0;
+
+        while (cont < 3) {
+            cont++;
+            System.out.println("Insira uma nota: ");
+            int notaAtual;
+            notaAtual = sc.nextInt();
+            notas += notaAtual;
+        }
+
+        System.out.println("A média entre as " + cont + " notas, é: " + notas / cont);
+        sc.close();
     }
 }
