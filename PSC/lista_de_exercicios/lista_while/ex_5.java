f42b5a3906b22a240caa24ecb87a6403e63f3b45
VictorHFMartins <navictor70@gmail.com>
2025-03-21 17:02:11 -0300

    Resolução do quinto exercício da lista de while.

--- PSC/lista_de_exercicios/lista_while/ex_5.java
+++ PSC/lista_de_exercicios/lista_while/ex_5.java
@@ -1,7 +1,22 @@
+// Faça um programa que peça ao usuário um número entre 12 e 20. Se a pessoa digitar um número diferente, mostrar a mensagem "entrada inválida" e solicitar o número novamente. Se digitar correto mostrar o número digitado.
+
 package PSC.lista_de_exercicios.lista_while;
+
+import java.util.Scanner;
 
 public class ex_5 {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("digite um número entre o intervalo de 12 a 20:");
+        int entrada = sc.nextInt();
+
+        while (entrada < 12 || entrada > 20) {
+            System.out.println("Entrada inválida.");
+            System.out.println("Digite novamente: ");
+            entrada = sc.nextInt();
+        }
+
+        System.out.println("Seu número é: " + entrada);
     }
 }
