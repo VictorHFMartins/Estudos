ef6024e6324f65eb4da7dd9383cc947946bda037
VictorHFMartins <navictor70@gmail.com>
2025-03-21 16:07:27 -0300

    Resolução do primeiro exercício da tabela de while, e criação de todos os arquivos de exercícios.

--- PSC/lista_de_exercicios/lista_while/ex_1.java
+++ PSC/lista_de_exercicios/lista_while/ex_1.java
@@ -1,7 +1,15 @@
+// Crie um programa que imprima 11 vezes a frase " Hello World!" utilizando uma estrutura sequencial e uma estrutura de repetição while.
+
 package PSC.lista_de_exercicios.lista_while;
 
 public class ex_1 {
     public static void main(String[] args) {
-        System.out.println("hello world");
+
+        int cont = 0;
+
+        while(cont < 11){
+            cont++;
+            System.out.println(cont +"º," + " Hello world");
+        }
     }
 }
---
+++ PSC/lista_de_exercicios/lista_while/ex_7.java
@@ -0,0 +1,7 @@
+package PSC.lista_de_exercicios.lista_while;
+
+public class ex_7 {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
