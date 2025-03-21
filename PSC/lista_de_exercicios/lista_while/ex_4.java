7de8e85b631b537f833c671a713baaf635916821
VictorHFMartins <navictor70@gmail.com>
2025-03-21 16:56:07 -0300

    Resolução do quarto exercício da lista de while.

--- PSC/lista_de_exercicios/lista_while/ex_4.java
+++ PSC/lista_de_exercicios/lista_while/ex_4.java
@@ -1,7 +1,29 @@
+// Escreva um programa com um looping indeterminado em que enquanto o usuário não pedir para o programa sair, ele continua printando um contador na tela.
+
 package PSC.lista_de_exercicios.lista_while;
+
+import java.util.Scanner;
 
 public class ex_4 {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        Scanner sc = new Scanner(System.in);
+
+        int cont = 0;
+        boolean key = true;
+
+        while(key){
+
+            System.out.println("Digite qualquer tecla para continuar o programa \nou digite encerrar para finalizar.");
+            String resp = sc.nextLine().trim().toLowerCase();
+
+            if (resp.equals("encerrar")) {
+                key = false;
+                System.out.println("Você encerrou o programa.");
+            }else{
+                cont ++;
+                System.out.println(cont);}
+        }
+
+        sc.close();
     }
 }
