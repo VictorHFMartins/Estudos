e537d157cd197576ba3dcd495f54ce5798eb13d5
VictorHFMartins <navictor70@gmail.com>
2025-03-21 16:09:57 -0300

    Resolução do segundo exercício da lista de while.

--- PSC/lista_de_exercicios/lista_while/ex_2.java
+++ PSC/lista_de_exercicios/lista_while/ex_2.java
@@ -1,7 +1,13 @@
+// Escrever um programa para exibir os números pares de 1 até 50 na tela.
+
 package PSC.lista_de_exercicios.lista_while;
 
 public class ex_2 {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        int cont = 0;
+        while (cont < 50) {
+            cont++;
+            System.out.print(cont + ", ");
+        }
     }
 }
