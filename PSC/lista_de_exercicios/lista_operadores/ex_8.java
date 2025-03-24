ab3e672bb425c7154042b9e80cbe1c1dad81c9cc
VictorHFMartins <navictor70@gmail.com>
2025-03-24 18:43:12 -0300

    Resolução do oitavo exercício da lista de operadores.

--- PSC/lista_de_exercicios/lista_operadores/ex_8.java
+++ PSC/lista_de_exercicios/lista_operadores/ex_8.java
@@ -1,5 +1,44 @@
+// Peça ao usuário inserir seu ano de nascimento e imprima sua idade;
+
 package PSC.lista_de_exercicios.lista_operadores;
 
+import java.time.LocalDate;
+import java.util.Scanner;
+
 public class ex_8 {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int hoje = LocalDate.now().getYear();
+        String repeat;
+
+        do {
+            int input = 0;
+            boolean value = false;
+
+            while (!value) {
+                System.out.println("Insira o seu ano de nascimento: (19xx - 2xxx) ");
+                String read = sc.nextLine().trim();
+
+                if (read.matches("^\\d{4}$") || read.matches("^\\d{1,2}\\.\\d{3}$")) {
+                    try {
+                        input = Integer.parseInt(read.replace(".", ""));
+                        value = true;
+
+                    } catch (NumberFormatException e) {
+                        System.out.println("Entrada inválida.");
+                    }
+                } else {
+                    System.out.println("Entrada inválida.");
+                }
+            }
+
+            System.out.println("Caso você ja tenha feito aniversário esse ano, você terá: " + (hoje - input) + " anos.");
+
+            System.out.println("Pressione x para tentar novamente, ou qualquer outra tecla para finalizar...");
+            repeat = sc.nextLine().trim().toLowerCase();
+
+        } while (repeat.equals("x"));
+
+        sc.close();
+    }
 }
