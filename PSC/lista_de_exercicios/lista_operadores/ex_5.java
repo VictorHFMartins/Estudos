9da25db52d3b618986efeb3e9eb2a5b1ea4d6adb
VictorHFMartins <navictor70@gmail.com>
2025-03-24 14:16:36 -0300

    Resolução do quinto exercício da lista de operadores.

--- PSC/lista_de_exercicios/lista_operadores/ex_5.java
+++ PSC/lista_de_exercicios/lista_operadores/ex_5.java
@@ -1,6 +1,39 @@
+// Peça ao usuário inserir a temperatura em °C e imprima a temperatura em °F:
+// F=C*95+32
+
 package PSC.lista_de_exercicios.lista_operadores;
+
+import java.util.Scanner;
 
 public class ex_5 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String repeat;
+        do {
+            double input = 0;
+            boolean value = false;
+
+            while (!value) {
+                System.out.println("Insira a temperatura em ºC: (Insira somente os números)");
+                String read = sc.nextLine().trim();
+
+                try {
+                    input = Double.parseDouble(read);
+                    value = true;
+
+                } catch (NumberFormatException e) {
+                    System.out.println("Entrada inválida.");
+                }
+
+            }
+            Double conversao = input * (9.0 / 5.0) + 32;
+            System.out.println("A temperatura de " + input + "ºC, em Fahrenheit é: " + String.format("%.2f", conversao) + "ºF");
+
+            System.out.println("Pressione x para tentar novamente, ou qualquer outra tecla para finalizar...");
+            repeat = sc.nextLine().trim().toLowerCase();
+
+        } while (repeat.equals("x"));
+
+        sc.close();
     }
 }
