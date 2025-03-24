a26ae5632c16cae68fcf96751a10f743788acc9b
VictorHFMartins <navictor70@gmail.com>
2025-03-24 19:01:18 -0300

    Resolução do nono exercício da lista de operadores.

--- PSC/lista_de_exercicios/lista_operadores/ex_9.java
+++ PSC/lista_de_exercicios/lista_operadores/ex_9.java
@@ -1,5 +1,40 @@
+// Faça um programa que calcula a área de uma circunferência com precisão de 2 casas decimais.
+
 package PSC.lista_de_exercicios.lista_operadores;
 
+import java.util.Scanner;
+
 public class ex_9 {
-    
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String repeat;
+
+        do {
+            int raio = 0;
+            boolean value = false;
+
+            while (!value) {
+                System.out.println("Insira o raio da circunferência em centímetros: (Somente números)");
+                String read = sc.nextLine().trim();
+
+                try {
+                    raio = Integer.parseInt(read);
+                    value = true;
+
+                } catch (NumberFormatException e) {
+                    System.out.println("Entrada inválida.");
+                }
+            }
+
+            double calculo = Math.PI * Math.pow(raio, 2);
+            System.out.println("A área da circunferência é: " + String.format("%.2f", calculo) + "cm.");
+
+            System.out.println("Pressione x para tentar novamente, ou qualquer outra tecla para finalizar...");
+            repeat = sc.nextLine().trim().toLowerCase();
+
+        } while (repeat.equals("x"));
+
+        sc.close();
+    }
 }
